217614ae8395daa6fbda96832d29d25b6d47edc6
Sinh Pham <phamansinh@gmail.com>
2016-01-16 00:32:18 -0800

    Var stack.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,4 +1,5 @@
 use parser::*;
+use std::collections::HashMap;
 
 pub struct Interpreter<'a> {
     ast: AstNode<'a>,
@@ -16,7 +17,13 @@ impl<'a> Interpreter<'a> {
         Self::visit(&self.ast);
     }
     
-    fn visit(node: &AstNode<'a>) -> Option<i32> {
+    fn visit(node: &AstNode<'a>) {
+        let mut variables: HashMap<String, i32> = HashMap::new();
+        let mut var_stack = vec![variables];
+        Self::visit_impl(node, &mut var_stack);
+    }
+    
+    fn visit_impl(node: &AstNode<'a>, var_stack: &mut Vec<HashMap<String, i32>>) -> Option<i32> {
         match *node {
             AstNode::Number(num) => Some(num),
             AstNode::Ident(ref s) => {
@@ -24,11 +31,11 @@ impl<'a> Interpreter<'a> {
                 None
             },
             AstNode::Factor(ref n) => {
-                Self::visit(n)
+                Self::visit_impl(n, var_stack)
             }
             AstNode::Term{ref factors, ref ops} => {
                 let first_op = [BiOp::Mul];
-                let v = factors.iter().map(|f| Self::visit(f)).zip(first_op.iter().chain(ops));
+                let v = factors.iter().map(|f| Self::visit_impl(f, var_stack)).zip(first_op.iter().chain(ops));
                 
                 let ret = v.fold(1, |acc, (val, op)| {
                     match *op {
@@ -47,9 +54,9 @@ impl<'a> Interpreter<'a> {
                 let (first_val, r_terms) = if terms.len() == signs.len() {
                     (0, terms.iter().skip(0))
                 } else {
-                    (Self::visit(&terms[0]).unwrap(), terms.iter().skip(1))
+                    (Self::visit_impl(&terms[0], var_stack).unwrap(), terms.iter().skip(1))
                 };
-                let v = r_terms.map(|f| Self::visit(f)).zip(signs);
+                let v = r_terms.map(|f| Self::visit_impl(f, var_stack)).zip(signs);
                 
                 let ret = v.fold(first_val, |acc, (val, op)| {
                     match *op {
@@ -62,31 +69,37 @@ impl<'a> Interpreter<'a> {
             },
             AstNode::BeginEnd(ref statements) => {
                 for s in statements {
-                    Self::visit(s);
+                    Self::visit_impl(s, var_stack);
                 }
                 None
             },
             AstNode::Assignment {ref ident, ref expression} => {
                 println!("assign called");
-                let ex_ret = Self::visit(expression);
+                let ex_ret = Self::visit_impl(expression, var_stack);
                 None
             }
             AstNode::Block{ref const_decl, ref var_decl, ref procedures, ref statement} => {
                 for c_decl in const_decl {
-                    Self::visit(c_decl);
+                    Self::visit_impl(c_decl, var_stack);
                 }
                 for v_decl in var_decl {
                     Self::visit_var(v_decl);
                 }
                 for p in procedures {
-                    Self::visit(p);
+                    Self::visit_impl(p, var_stack);
                 }
-                Self::visit(statement);
+                Self::visit_impl(statement, var_stack);
                 println!("block");
                 None
             }
             AstNode::Const{ref ident, ref value} => {
                 println!("const");
+                let curr_scope = var_stack.last_mut().unwrap();
+                
+                let ident = Self::get_ident(ident);
+                let val = Self::get_number(value);
+                
+                curr_scope.insert(ident, val);
                 None
             }
             AstNode::Procedure {ref ident, ref block} => {
@@ -100,6 +113,22 @@ impl<'a> Interpreter<'a> {
     fn visit_var(node: &AstNode<'a>) {
         if let &AstNode::Ident(s) = node {
             println!("var {:?}", s);
+        }
+    }
+    
+    fn get_ident(node: &AstNode<'a>) -> String {
+        if let &AstNode::Ident(s) = node {
+            s.to_owned()
+        } else {
+            "".to_owned()
+        }
+    }
+    
+    fn get_number(node: &AstNode<'a>) -> i32 {
+        if let &AstNode::Number(n) = node {
+            n
+        } else {
+            panic!("asdf");
         }
     }
 }
