4195f2b015214b8956ae682e593eab7fe5e61c5a
Sinh Pham <phamansinh@gmail.com>
2016-01-27 23:08:30 -0800

    Regex lexer.

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,11 +1,9 @@
 use regex::Regex;
 
 use chomp::*;
-//use chomp::parsers::Error;
 use std::str;
 use std::cell::Cell;
 use std::collections::HashSet;
-use std::io;
 
 #[derive(Debug, Clone, Copy)]
 pub enum Token<'a> {
@@ -15,145 +13,7 @@ pub enum Token<'a> {
     Separator(&'a str),
 }
 
-fn number(i: Input<u8>) -> U8Result<Token> {
-    parse!{i;
-        let num = take_while1(|c| (c as char).is_digit(10));
-
-        ret Token::Number(str::from_utf8(num).unwrap().parse::<i32>().unwrap())
-    }
-}
-
-fn ident(i: Input<u8>) -> U8Result<Token> {
-    let first_run = Cell::new(true);
-    
-    parse!{i;
-        let rest = take_while(|c| {
-            if first_run.get() {
-                first_run.set(false);
-                (c as char).is_alphabetic() || c == b'_'
-            } else {
-                (c as char).is_alphabetic() || (c as char).is_digit(10)
-            }
-        });
-
-        ret Token::Ident(str::from_utf8(rest).unwrap())
-    }
-}
-
-fn keyword(i: Input<u8>) -> U8Result<Token> {
-    fn to_kw(s: &[u8]) -> Token {
-        Token::Keyword(str::from_utf8(s).unwrap())
-    }
-    
-    fn all_kw(i: Input<u8>) -> U8Result<Token> {
-        
-        parse!{i;
-            let r = string(b"BEGIN")
-                <|> string(b"END")
-                <|> string(b"PROCEDURE")
-                <|> string(b"WHILE")
-                <|> string(b"DO")
-                <|> string(b"IF")
-                <|> string(b"THEN")
-                <|> string(b"CALL")
-                <|> string(b"ODD")
-                <|> string(b"VAR")
-                <|> string(b"CONST");
-                
-            ret to_kw(r)
-        }
-    }
-    
-    parse!{i;
-        let kw = all_kw();
-
-        ret kw
-    }
-}
-
-fn separator(i: Input<u8>) -> U8Result<Token> {
-    fn to_sep(s: &[u8]) -> Token {
-        Token::Separator(str::from_utf8(s).unwrap())
-    }
-    
-    fn all_sep(i: Input<u8>) -> U8Result<Token> {
-        parse!{i;
-            let r = string(b":=")
-                <|> string(b">=")
-                <|> string(b"<=")
-                <|> string(b",")
-                <|> string(b";")
-                <|> string(b"=")
-                <|> string(b">")
-                <|> string(b"<")
-                <|> string(b"+")
-                <|> string(b"-")
-                <|> string(b"*")
-                <|> string(b"/")
-                <|> string(b"#")
-                <|> string(b".")
-                <|> string(b"!")
-                <|> string(b"(")
-                <|> string(b")");
-            
-            ret to_sep(r)
-        }
-    }
-    
-    parse!{i;
-        let sep = all_sep();
-
-        ret sep
-    }
-}
-
-pub fn run_lexer(i: Input<u8>) -> U8Result<Vec<Token>> {
-    fn is_token(i: Input<u8>) -> U8Result<Token> {
-        // println!("tok called");
-        // println!("i = {:?}", i);
-        // let mut input_text = String::new();
-        // io::stdin()
-        //             .read_line(&mut input_text);
-        parse!{i;
-            let r = keyword() <|> separator() <|> number() <|> ident();
-
-            ret r
-        }
-    }
-    
-    fn is_not_token(i: Input<u8>) -> U8Result<()> {
-        // println!("not tok called");
-        // println!("i = {:?}", i);
-        // let mut input_text = String::new();
-        // io::stdin()
-        //             .read_line(&mut input_text);
-        parse!{i;
-            let _ = take_while(|c| (c as char).is_whitespace());
-
-            ret ()
-        }
-    }
-    
-    fn all_token(i: Input<u8>) -> U8Result<Token> {
-        parse!{i;
-            let _ = option(is_not_token, ());
-            let tok = is_token();
-
-            ret tok
-        }
-    }
-    
-    parse!{i;
-        //let _ = take_while(|c| (c as char).is_whitespace());
-        let tokens = many(all_token);
-
-        ret tokens
-    }
-}
-
-
-
-pub fn r_lexer(input: &str) -> Vec<Token> {
+pub fn r_lexer(input: &str) -> Result<Vec<Token>, String> {
     fn r_number(input: &str) -> Option<(Token, usize, usize)> {
         let re = Regex::new(r"^\d+").unwrap();
         
@@ -217,14 +77,11 @@ pub fn r_lexer(input: &str) -> Vec<Token> {
     let mut curr_idx:usize = 0;
     let mut curr_str = &input[curr_idx..];
     
-    // let m_funcs = vec![&r_keyword, &r_sep, &r_number, Box::new(r_ident)];
-    
     let f1 = &r_ident_keyword;
     let f2 = &r_number;
     let f3 = &r_sep;
     
     let d = f1(input);
-    // println!("d = {:?}", d);
     
     let mut m_funcs: Vec<&Fn(&str) -> Option<(Token, usize, usize)>> = Vec::new();
     
@@ -233,31 +90,29 @@ pub fn r_lexer(input: &str) -> Vec<Token> {
     m_funcs.push(f3);
     
     while !curr_str.is_empty() {
-        // println!("while curr_str = {:?}", curr_str);
+        let mut progressed = false;
         
         if let Some((_, non_empty)) = r_whitespace(curr_str) {
             curr_idx += non_empty;
             curr_str = &input[curr_idx..];
-            // println!("ws curr_str = {:?}", curr_str);
         }
         
         for m_func in &m_funcs {
-            // println!("call f");
             if let Some((t, _, n_start)) = m_func(curr_str) {
                 curr_idx += n_start;
                 curr_str = &input[curr_idx..];
-                // println!("f curr_str = {:?}", curr_str);
                 r.push(t);
+                progressed = true;
                 break;
             }
         }
-//         let mut input = String::new();
-// 
-//         (io::stdin().read_line(&mut input));
-        //panic!("shouldn't be here");
+        
+        if !progressed {
+            return Err("can not progress".to_string());
+        }
     }
     
-    r
+    Ok(r)
 }
 
 #[test]
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ mod parser;
 mod codegen;
 mod interpreter;
 
-use regex::Regex;
 use lexer::*;
 use parser::*;
 use interpreter::*;
@@ -17,30 +16,89 @@ use interpreter::*;
 fn main() {
 
   
-    let tokens = r_lexer("VAR x, squ;
+    let tokens = r_lexer("
+    
+CONST
+  m =  7,
+  n = 85;
 
-PROCEDURE square;
+VAR
+  x, y, z, q, r;
+
+PROCEDURE multiply;
+VAR a, b;
+
 BEGIN
-   squ:= x * x
+  a := x;
+  b := y;
+  z := 0;
+  WHILE b > 0 DO BEGIN
+    IF ODD b THEN z := z + a;
+    a := 2 * a;
+    b := b / 2
+  END
+END;
+
+PROCEDURE divide;
+VAR w;
+BEGIN
+  r := x;
+  q := 0;
+  w := y;
+  WHILE w <= r DO w := 2 * w;
+  WHILE w > y DO BEGIN
+    q := 2 * q;
+    w := w / 2;
+    IF w <= r THEN BEGIN
+      r := r - w;
+      q := q + 1
+    END
+  END
+END;
+
+PROCEDURE gcd;
+VAR f, g;
+BEGIN
+  f := x;
+  g := y;
+  WHILE f # g DO BEGIN
+    IF f < g THEN g := g - f;
+    IF g < f THEN f := f - g
+  END;
+  z := f
 END;
 
 BEGIN
-   x := 1;
-   WHILE x <= 10 DO
-   BEGIN
-      CALL square;
-      ! squ;
-      x := x + 1
-   END
+  x := m;
+  y := n;
+  CALL multiply;
+  !z;
+  
+  x := 25;
+  y :=  3;
+  CALL divide;
+  !r;
+  !q;
+  
+  x := 84;
+  y := 36;
+  CALL gcd;
+  
+  !z;
 END.");
 
-
-    let ast = parse_only(program, &tokens);
+    // println!("tokens = {:?}", tokens);
     
-    if let Ok(c) = ast {
-        let interpreter = Interpreter::new(c);
-        let ret = interpreter.run();
-        println!("ret = {:?}", ret);
+    if let Ok(tokens) = tokens {
+        let ast = parse_only(program, &tokens);
+        
+        if let Ok(c) = ast {
+            // println!("c = {:?}", c);
+            
+            let interpreter = Interpreter::new(c);
+            let ret = interpreter.run();
+            println!("ret = {:?}", ret);
+        }
     }
     
 }
